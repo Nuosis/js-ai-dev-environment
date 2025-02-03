1f04482dcad78cbe8535afc63a90e014460dfdb0
integrating-magic <jeremy@integratingmagic.io>
2025-02-03 17:28:46 -0500

    final updates for new version

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-// import "./style.css";
 import { sampleData } from "./sampleData";
 const btn = document.querySelector("button");
 
+// the JS button
 btn.onclick = function () {
   console.log("You ran some JavaScript");
   alert("You ran some JavaScript");
 };
-window.loadTable = function () {
-  const table = document.createElement("table");
-  const tbody = document.createElement("tbody");
-  sampleData.forEach((rowData, i) => {
-    const row = document.createElement("tr");
-    const nameCell = document.createElement("td");
-    const emailCell = document.createElement("td");
-    nameCell.textContent = rowData.name;
-    emailCell.textContent = rowData.email;
-    row.style.backgroundColor = i % 2 === 0 ? "lightgray" : "white";
-    row.appendChild(nameCell);
-    row.appendChild(emailCell);
-    tbody.appendChild(row);
-  });
 
-  table.appendChild(tbody);
-  document.body.appendChild(table);
+// this funciton is called by FileMaker.
+window.loadWidget = function () {
+  console.log("FileMaker called this function");
 };
